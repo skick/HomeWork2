66b3c86e07891eea4d5d0c3678767a85cdc3e0ad
Jonne Taipale <skick@Jonne-MacBook-Air-10.local>
2020-01-27 16:16:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.homework2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        time_button.setOnClickListener {
+            startActivity(Intent(applicationContext, TimeActivity::class.java))
+        }
+
+        map_button.setOnClickListener {
+            startActivity(Intent(applicationContext, MapActivity::class.java))
+        }
+
+        test_button.setOnClickListener {
+            startActivity(Intent(applicationContext, TestActivity::class.java))
+        }
+    }
+}
