9bfeda932359a0e0cae614d1e16e9643686035a6
skick <jonne.taipale@gmail.com>
2020-03-09 18:46:21 +0200

    Lab 4, Lab 5 in progress

--- app/src/main/java/com/example/homework2/MainActivity.kt
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.homework2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,9 +26,29 @@ class MainActivity : AppCompatActivity() {
         test_button.setOnClickListener {
             startActivity(Intent(applicationContext, TestActivity::class.java))
         }
+    }
 
-        val data = arrayOf("Oulu", "Helsinki", "Tampere", "Rovaniemi")
-        val reminderAdapter = ReminderAdapter(applicationContext, data)
-        list.adapter = reminderAdapter
+    override fun onResume() {
+        super.onResume()
+        refreshlist()
+    }
+
+    private fun refreshlist() {
+        doAsync {
+            val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "reminders").build()
+            val reminders = db.reminderDao().getReminders()
+            db.close()
+
+            uiThread {
+
+                if(reminders.isNotEmpty()) {
+                    val adapter = ReminderAdapter(applicationContext, reminders)
+                    list.adapter = adapter
+
+                } else {
+                    toast("No reminders yet")
+                }
+            }
+        }
     }
 }
