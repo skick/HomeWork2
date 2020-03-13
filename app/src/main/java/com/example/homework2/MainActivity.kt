117d6dc4df12557e77fbb663302fed2a531a0548
skick <jonne.taipale@gmail.com>
2020-03-13 17:13:43 +0200

    Final commit

--- app/src/main/java/com/example/homework2/MainActivity.kt
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.homework2
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
+import java.util.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,10 +36,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        refreshlist()
-    }
 
     private fun refreshlist() {
         doAsync {
@@ -49,6 +53,38 @@ class MainActivity : AppCompatActivity() {
                     toast("No reminders yet")
                 }
             }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        refreshlist()
+    }
+
+    private fun refreshList() {}
+
+    companion object {
+        val CHANNEL_ID="REMINDER_CHANNEL_1D"
+        var NotificationID = 1567
+        fun showNotification(context: Context, message:String){
+            var notificationBuilder= NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_alarm_24px).setContentTitle(context.getString(R.string.app_name))
+                .setContentTitle("Reminder").setContentText((message))
+                .setStyle(NotificationCompat.BigTextStyle().bigText(message))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            var notificationManager=
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    CHANNEL_ID,
+                    context.getString(R.string.app_name),
+                    NotificationManager.IMPORTANCE_DEFAULT
+                ).apply { description = context.getString(R.string.app_name) }
+                notificationManager.createNotificationChannel(channel)
+            }
+            val notification = NotificationID + Random(NotificationID).nextInt(1, 30)
+            notificationManager.notify(notification, notificationBuilder.build())
         }
     }
 }
